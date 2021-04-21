e1b86125ff3726c71b5138d0561fcb07ea3aaf53
Marcus Filipsson <macke2002@outlook.com>
2021-04-21 09:09:41 +0200

    class car

---
+++ Car app/Car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_app
+{
+    class Car
+    {
+        private string regNr;
+        private string make;
+        private string model;
+        private int year;
+        private bool forSale;
+
+        public Car(string regNr, string make, string model, int year, bool forSale)
+        {
+            this.regNr = regNr;
+            this.make = make;
+            this.model = model;
+            this.year = year;
+            this.forSale = forSale;
+        }
+
+        public string GetRegNr()
+        {
+            return regNr;
+        }
+
+        public void SetRegNr (string regNr)
+        {
+            this.regNr = regNr;
+        }
+
+        public string GetMake()
+        {
+            return make;
+        }
+
+        public void SetMake(string make)
+        {
+            this.make = make;
+        }
+    }
+}
