9519ad0a27f29871112ed32f99f8d0a8917d33f5
Marcus Filipsson <macke2002@outlook.com>
2021-04-22 11:57:32 +0200

    Database First

---
+++ Car app/Database.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Car_app
+{
+    class Database
+    {
+        public SQLiteConnection dbConn;
+        private string databaseFilename = "./car.db";
+
+        public Database()
+        {
+            if (!File.Exists(databaseFilename))
+            {
+                File.Create(databaseFilename);
+            }
+
+            dbConn = new SQLiteConnection("Data Source=" + databaseFilename);
+        }
+
+        public void OpenConnection()
+        {
+
+        }
+    
+
+        
+        
+    }
+    
+}
